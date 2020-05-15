b614581dbdb33452fe59b121c0095c28ec694a4d
David Lewis <mail@david-lewis.com>
2020-05-15 14:16:20 +1000

    Add basic Vue setup

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
-console.log('app.js running');
+import Vue from 'vue/dist/vue.js';
+
+Vue.component('kc-nav', {
+    data() {
+      return {
+        message: 'hello'
+      }
+    },
+    methods: {
+        toggleNav() {
+            console.log('hi there');
+        }
+    },
+    mounted() {
+        console.log('boo');
+    } 
+});
+
+const app = new Vue({
+  el: '#site-heading'
+});
