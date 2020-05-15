df5d57e21835d199343a6a243baad29837773109
David Lewis <mail@david-lewis.com>
2020-05-15 11:43:18 +1000

    Plug item hero into Wordpress

--- src/Widgets/ItemHero.php
+++ src/Widgets/ItemHero.php
@@ -21,8 +21,19 @@ class ItemHero extends \WP_Widget
     public function widget($args, $instance)
     {
         $title = apply_filters('widget_title', $instance['title']);
+        $subtitle = $instance['subtitle'];
+        $url = $instance['url'];
+        $linktitle = $instance['linktitle'];
+        $imageUrl = wp_get_attachment_url($instance['photo']);
+
         if (!empty($title)) {
-            echo $title;
+            \Timber::render( 'itemhero.twig', array(
+                'title' => $title,
+                'subtitle' => $subtitle,
+                'url' => $url,
+                'imageUrl' => $imageUrl,
+                'linktitle' => $linktitle
+            ));
         }
     }
 
@@ -32,11 +43,13 @@ class ItemHero extends \WP_Widget
         $subtitle = esc_attr((isset($instance['subtitle'])) ? $instance['subtitle'] : '');
         $photo = (isset($instance['photo'])) ? $instance['photo'] : '';
         $url = (isset($instance['url'])) ? $instance['url'] : '';
+        $linktitle = (isset($instance['linktitle'])) ? $instance['linktitle'] : '';
         $imageUrl = wp_get_attachment_url($photo);
 
         echo Forms::text($this->get_field_id('title'), $this->get_field_name('title'), 'Title', $title);
         echo Forms::text($this->get_field_id('subtitle'), $this->get_field_name('subtitle'), 'Subtitle', $subtitle);
-        echo Forms::text($this->get_field_id('url'), $this->get_field_name('url'), 'url:', $url);
+        echo Forms::text($this->get_field_id('linktitle'), $this->get_field_name('linktitle'), 'Link title', $linktitle);
+        echo Forms::text($this->get_field_id('url'), $this->get_field_name('url'), 'Link URL', $url);
         echo Forms::imageUpload($this->get_field_id('photo'), $this->get_field_name('photo'), '', $photo, $imageUrl);
     }
 
@@ -46,6 +59,7 @@ class ItemHero extends \WP_Widget
         $instance['title'] = (!empty($new_instance['title'])) ? strip_tags($new_instance['title']) : '';
         $instance['subtitle'] = (!empty($new_instance['subtitle'])) ? strip_tags($new_instance['subtitle']) : '';
         $instance['photo'] = (!empty($new_instance['photo'])) ? strip_tags($new_instance['photo']) : '';
+        $instance['linktitle'] = (!empty($new_instance['linktitle'])) ? strip_tags($new_instance['linktitle']) : '';
         $instance['url'] = (!empty($new_instance['url'])) ? strip_tags($new_instance['url']) : '';
         return $instance;
     }
